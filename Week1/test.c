6cf8e346f00787a632337baf81d53c1ddd2bc646
maodreamer <maodreamer@gmail.com>
2021-09-23 22:09:27 +0200

    some practice

--- Week1/test.c
+++ Week1/test.c
@@ -8,15 +8,16 @@ int main(
 )
 
 {
-    int *asentries = (int*) malloc(3*4*sizeof(int));
-
-    for ( int ix = 0; ix < 3; ++ix ){
-        printf("%i\n", asentries);
-        printf("%i\n", asentries+1);
-    }
+    int a = 1;
     
+    int *ap = &a;
+    printf("%i\n", (*ap+1));
 
-    //   int *a = asentries + 4*ix;
-    //   printf("%i", as[ix]);
+
+
+
+
+    // printf("%i\n", &a);
+    // printf("%i\n", *(&a));
 
 }
