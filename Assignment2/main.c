640d89abd690a130d54ca25f15729daa41fa8a4e
wenli <wenli@student.chalmers.se>
2021-09-30 17:22:37 +0200

    assignment 2, frame

---
+++ Assignment2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    FILE * pFile;
+    pFile = fopen ("cells" , "r+");
+    if (pFile == NULL) {
+        perror ("Error opening file");
+        return 1;
+    }
+
+    // TODO: read from file and parse input
+
+    // TODO: computer distance
+
+    fclose (pFile);
+    return 0;
+}
