dbdffa3538f59e58cc4be24e94018084739c8487
Zhuqi Xiao <zqxiao.shawn@qq.com>
2021-10-05 15:29:06 +0200

    update assignment 2 by Shawn

--- Assignment2/cell_distances.c
+++ Assignment2/cell_distances.c
@@ -8,6 +8,9 @@
 #include <string.h>
 #include <time.h>
 
+// A GLOBAL VAR FOR USER DEFINE BATCH_SIZE
+#define BATCH_SIZE 5
+
 int main(int argc, char* argv[])
 {
     // counter vector for tracking counted numbers
@@ -28,7 +31,7 @@ int main(int argc, char* argv[])
     char filename[] = "dis_file";
     FILE* file;
     file = fopen(filename, "r");
-    if ( file == NULL ) {
+    if (file == NULL) {
         printf("error opening file\n");
         return -1;
     }
@@ -39,35 +42,80 @@ int main(int argc, char* argv[])
     size_t line_num = (file_size+1)/24;
     printf("File size = %d\nLine number = %d\n", file_size, line_num);
 
-    // apply memory for storing read lines
-    size_t batch_size = 3;
+    long file_cursor = ftell(file); // for store the current file position
+    size_t batch_num;
+    size_t last_batch_size = line_num % BATCH_SIZE;
+    batch_num = (last_batch_size == 0) ? line_num/BATCH_SIZE : line_num/BATCH_SIZE + 1;
+    size_t batch_size = (batch_num==1) ? line_num : BATCH_SIZE;
+    printf("Batch size = %d\nBatch num = %d\n", batch_size, batch_num);
+
+    // allocate memory for storing read lines
+    
     int (*batch_1)[3] = (int (*)[3])malloc(batch_size * sizeof(int[3]));
     int (*batch_2)[3] = (int (*)[3])malloc(batch_size * sizeof(int[3]));
     fseek(file, 0, SEEK_SET); // to the start of the file
-    long file_cursor = ftell(file);
+    
+    char per_line[25]; // for storing a line's info
+    char temp_str[7]; // for string to integer
+    // Outer loop
+    for (size_t batch_out = 0; batch_out < batch_num; batch_out++){
+        printf("Outer loop: %d\n", batch_out); // FOR TESTING (delete later)
 
-    while (ftell(file) != file_size){
-        char per_line[25];
+        file_cursor = batch_out * batch_size * 24;
+        fseek(file, file_cursor, SEEK_SET);
+
+        // prepare batch_1 for outer loop
         for (size_t line = 0; line < batch_size; line++){
             fgets(per_line, 25, file);
             for (size_t ix = 0; ix < 3; ++ix) {
-                char temp_str[7];
-                memcpy(temp_str, &per_line[ix*8], 3);
-                memcpy(temp_str+3, &per_line[ix*8+4], 3);
+                memcpy(temp_str, &per_line[ix * 8], 3);
+                memcpy(temp_str+3, &per_line[ix * 8 + 4], 3);
                 temp_str[6] = '\0';
                 batch_1[line][ix] = (short)atoi(temp_str);
             }
-        }
+        } // end of prepare batch_1
 
-    }
-    for (size_t line = 0; line < batch_size; line++){
-        for (size_t ix = 0; ix < 3; ++ix) {
-            printf("%d\t", batch_1[line][ix]);
+        // FOR TESTING (delete later) print batch_1 
+        for (size_t line = 0; line < batch_size; line++){
+            for (size_t ix = 0; ix < 3; ++ix) {
+                printf("%d\t", batch_1[line][ix]);
+            }
+            printf("\n");
         }
-        printf("\n");
-    }
+        
+        fseek(file, file_cursor, SEEK_SET);
+        
+        // Inner loop
+        for (size_t batch_in = batch_out; batch_in < batch_num; batch_in++){
+            printf("\tInner loop: %d\n", batch_in); // FOR TESTING (delete later)
+            // prepare batch_2 for inner loop
+            for (size_t line = 0; line < batch_size; line++){
+                fgets(per_line, 25, file);
+                for (size_t ix = 0; ix < 3; ++ix) {
+                    memcpy(temp_str, &per_line[ix * 8], 3);
+                    memcpy(temp_str+3, &per_line[ix * 8 + 4], 3);
+                    temp_str[6] = '\0';
+                    batch_2[line][ix] = (short)atoi(temp_str);
+                }
+            } // end of prepare batch_2
+
+            // FOR TESTING (delete later) print batch_2 
+            for (size_t line = 0; line < batch_size; line++){
+                for (size_t ix = 0; ix < 3; ++ix) {
+                    printf("\t\t%d\t", batch_2[line][ix]);
+                }
+                printf("\n");
+            }
+
+            // TODO: calculate distances
+
+        } // end of inner loop
+    } // end of outerloop
+    
+
     // close the file
     fclose(file);
+    // free memory
     free(batch_1);
     free(batch_2);
 
