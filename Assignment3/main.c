3aca09c2c045c949ca7691070649dfb11930acd8
Zhuqi Xiao <zqxiao.shawn@qq.com>
2021-10-18 16:52:03 +0200

    parsing update

--- Assignment3/main.c
+++ Assignment3/main.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <threads.h>
 #include "common.h"
@@ -7,17 +8,23 @@
 
 const int ATTR_DEFAULT_VALUE = 0;
 
-int nsize = 10;
-int nthrds = 18;
-int exponent = 3;
+int nsize, nthrds, exponent;
 
-int main()
+int main(int argc, char *argv[])
 {
-    // TODO:
-    // Parsing command line arguments and store into 
-    //int nsize
-    //int nthrds
-    //int exponent
+    if (argc != 4){
+        fprintf(stderr, "Error: Wrong Syntax. Should be in form:\n./newton -t[number_of_thread] -l[size_of_image] [exponent]\n");
+        exit(0);
+    }
+    char *ptr_t, *ptr_l;
+    for (size_t ix = 1; ix < argc; ix++)
+    {
+        ptr_t = strchr(argv[ix], 't');
+        ptr_l = strchr(argv[ix], 'l');
+        if (ptr_t) nthrds = strtol(++ptr_t, NULL, 10);
+        else if (ptr_l) nsize = strtol(++ptr_l, NULL, 10);
+        else exponent = strtol(argv[ix], NULL, 10);
+    }
 
     TYPE_ROW *result = (TYPE_ROW *)malloc(nsize * sizeof(TYPE_ROW));
     for (int ix = 0; ix < nsize; ++ix) {
